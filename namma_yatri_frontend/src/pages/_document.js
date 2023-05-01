15901d3085a24278f5df1363b2748b03132971f2
vijay-kumar-yadav <vky405021@gmail.com>
2023-05-01 22:51:46 +0530

    background image fix

--- namma_yatri_frontend/src/pages/_document.js
+++ namma_yatri_frontend/src/pages/_document.js
@@ -1,13 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+    body {
+      background-image: url('/assets/background.gif');
+    }
+  `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
